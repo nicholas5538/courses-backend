3c3ba30c6b282ef3bc0633d0fd3e8d8195027eff
Nicholas <nicholas8399@gmail.com>
2024-02-14 13:04:35 +0800

    Single table inheritance implementation

--- src/main/java/com/nicholascoding/jpa/models/Resource.java
+++ src/main/java/com/nicholascoding/jpa/models/Resource.java
@@ -4,13 +4,18 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
 @Entity
-public class Resource extends BaseEntity {
+// Single table inheritance
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "resource_type")
+public class Resource {
+    @Id
+    @GeneratedValue
+    private Integer id;
     @Column(nullable = false)
     private String name;
     private int size;
