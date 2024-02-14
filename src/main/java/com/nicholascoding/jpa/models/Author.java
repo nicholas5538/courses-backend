384edde884b33239301d6d8a28b0fad7431745f7
Nicholas <nicholas8399@gmail.com>
2024-02-14 11:06:50 +0800

    Create entity for Author

---
+++ src/main/java/com/nicholascoding/jpa/models/Author.java
@@ -0,0 +1,28 @@
+package com.nicholascoding.jpa.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "AUTHOR_TBL")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, updatable = false, length = 36)
+    private String id;
+    @Column(name="f_name", nullable = false)
+    private String firstName;
+    @Column(name="l_name", nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private int age;
+}
